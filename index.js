5dc34458ab25169a91c0dbc029ec6a82e84e5ee7
Cherkashin Alexander <cherkalexander@gmail.com>
2022-07-24 22:07:03 +0300

    create completeTasks function

--- index.js
+++ index.js
@@ -4,69 +4,82 @@ require('dotenv').config()
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
 (async () => {
-    const databaseId = process.env.DATABASE_ID;
-    // const now = new Date("2022-07-04T04:51:27.000Z");
-    const now = new Date()
-    const [todayDate] = now.toISOString().split('T');
-    
-    console.log(`ℹ️ Requesting tasks changed on ${now.toISOString()}`);
+  const date = new Date();
+  completeTasks(process.env.DATABASE_ID, date);
+})();
 
-    const modifiedToday = await notion.databases.query({
-      database_id: databaseId,
-      filter: {
-            "and": [
-              {
-                "property": "Modified At",
-                "date": {
-                    "equals": todayDate
-                }
-              },
-              {
-                "property": "Completed At",
-                "date": {
-                  "is_empty": true,
-                }
+// (async () => {
+//   const startDate = new Date("2022-04-19T04:51:27.000Z");
+//   const endDate = new Date("2022-07-24T04:51:27.000Z");
+//   const currentDate = new Date(startDate.toISOString());
+  
+//   while(currentDate < endDate) {
+//     await completeTasks(process.env.DATABASE_ID, currentDate);
+//     currentDate.setDate(currentDate.getDate() + 1);
+//   }
+// })();
+
+async function completeTasks(databaseId, date) {
+  const [todayDate] = date.toISOString().split('T');
+  
+  console.log(`ℹ️ Requesting tasks changed on ${date.toISOString()}`);
+
+  const modifiedToday = await notion.databases.query({
+    database_id: databaseId,
+    filter: {
+          "and": [
+            {
+              "property": "Modified At",
+              "date": {
+                  "equals": todayDate
               }
-            ]
-      },
-    });
-
-    const requests = modifiedToday.results.map(page => {
-        return notion.pages.properties.retrieve({
-            page_id: page.id, 
-            property_id: page.properties['List'].id
-        }).then((property) => {
-            return { page, property };
-        })
-    });
-
-    const pagesWithProperties = await Promise.all(requests);
-
-    console.log(`ℹ️ Tasks changed on ${now.toISOString()}`);
-    console.log(JSON.stringify(pagesWithProperties, null, 2));
-
-    // status may be null if default value is used for status
-    const completedToday = pagesWithProperties.filter(({property}) => property.status?.name.includes("Done"));
-    console.log(`🕙 Tasks completed today:`);
-    console.log(JSON.stringify(completedToday, null, 2));
-
-    const updateRequests = completedToday.map(({page}) => {
-      return notion.pages.update({
-        page_id: page.id,
-        properties: {
-          'Completed At': {
-            "date": {
-              "start": todayDate
+            },
+            {
+              "property": "Completed At",
+              "date": {
+                "is_empty": true,
+              }
             }
+          ]
+    },
+  });
+
+  const requests = modifiedToday.results.map(page => {
+      return notion.pages.properties.retrieve({
+          page_id: page.id, 
+          property_id: page.properties['List'].id
+      }).then((property) => {
+          return { page, property };
+      })
+  });
+
+  const pagesWithProperties = await Promise.all(requests);
+
+  console.log(`ℹ️ Tasks changed on ${date.toISOString()}`);
+  console.log(JSON.stringify(pagesWithProperties, null, 2));
+
+  // status may be null if default value is used for status
+  const completedToday = pagesWithProperties.filter(({property}) => property.status?.name.includes("Done"));
+  console.log(`🕙 Tasks completed today:`);
+  console.log(JSON.stringify(completedToday, null, 2));
+
+  const updateRequests = completedToday.map(({page}) => {
+    return notion.pages.update({
+      page_id: page.id,
+      properties: {
+        'Completed At': {
+          "date": {
+            "start": todayDate
           }
         }
-      }).then(
-        () => console.log(`✅ Page ${page.id} is updated`), 
-        () => console.log(`❌ Failed to update page ${page.id}`),
-      );
-    });
+      }
+    }).then(
+      () => console.log(`✅ Page ${page.id} is updated`), 
+      () => console.log(`❌ Failed to update page ${page.id}`),
+    );
+  });
 
-    await Promise.all(updateRequests);
+  await Promise.all(updateRequests);
 
-    console.log(`✅ Done`);
-  })();
+  console.log(`✅ Done`);
+}
