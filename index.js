06370dc6f192a21a48bd89445c8fa4bc3bb0a965
Cherkashin Alexander <cherkalexander@gmail.com>
2022-07-25 08:07:15 +0300

    change cron time

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 require('dotenv').config();
 const { completeTasks } = require('./completeTasks');
 
-console.log(`Database id: ${process.env.DATABASE_ID.length}`);
-
 (async () => {
   const date = new Date();
   completeTasks(process.env.DATABASE_ID, date);
