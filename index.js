6c2ac1c0f8a74986d99e50e628ad9a93cb2c970d
Cherkashin Alexander <cherkalexander@gmail.com>
2022-07-25 07:48:35 +0300

    Print database id

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const { completeTasks } = require('./completeTasks');
 
+console.log(`Database id: ${process.env.DATABASE_ID}`);
+
 (async () => {
   const date = new Date();
   completeTasks(process.env.DATABASE_ID, date);
