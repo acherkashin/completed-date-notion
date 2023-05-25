01f9f6105d1b119c9378e2030bb57a0e6a94928a
Cherkashin Alexander <cherkalexander@gmail.com>
2023-05-25 21:23:00 +0000

    Fix year

--- completeOldTasks.js
+++ completeOldTasks.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 (async () => {
   const startDate = new Date('2023-02-01T04:51:27.000Z');
-  const endDate = new Date('2022-05-26T04:51:27.000Z');
+  const endDate = new Date('2023-05-26T04:51:27.000Z');
   const currentDate = new Date(startDate.toISOString());
 
   while (currentDate < endDate) {
