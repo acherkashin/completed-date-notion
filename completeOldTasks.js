68b13a3748b77051a9d78ab71f36630fa26037ce
Cherkashin Alexander <cherkalexander@gmail.com>
2023-05-25 21:20:21 +0000

    Update start and end dates

--- completeOldTasks.js
+++ completeOldTasks.js
@@ -2,8 +2,8 @@ const { completeTasks } = require('./completeTasks');
 require('dotenv').config();
 
 (async () => {
-  const startDate = new Date('2022-04-19T04:51:27.000Z');
-  const endDate = new Date('2022-07-24T04:51:27.000Z');
+  const startDate = new Date('2023-02-01T04:51:27.000Z');
+  const endDate = new Date('2022-05-26T04:51:27.000Z');
   const currentDate = new Date(startDate.toISOString());
 
   while (currentDate < endDate) {
